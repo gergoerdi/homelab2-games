e5056b015ba3084fd947ca9cb42fea894931dc65
Gergo ERDI <gergo@erdi.hu>
2023-08-31 22:06:48 +0800

    Write output to `snake.obj`

--- src/main.hs
+++ src/main.hs
@@ -3,12 +3,42 @@
 module Main where
 
 import Z80
+import Data.Word
 import qualified Data.ByteString as BS
-import Text.Printf
+import Control.Monad
 
 main :: IO ()
 main = do
-    mapM_ (printf "%02x\n") $ BS.unpack $ asmData block
+    BS.writeFile "snake.obj" $ asmData block
   where
     block = org 0x50_00 do
-        ld A 0xff
+        clearScreen
+        _ <- ret
+        pure ()
+
+videoStart :: Word16
+videoStart = 0xc001
+
+numCols :: Word16
+numCols = 40
+
+numRows :: Word16
+numRows = 25
+
+loopB :: Z80ASM -> Z80ASM
+loopB body = do
+    rec loop <- label
+        body
+        djnz loop
+    pure ()
+
+clearScreen :: Z80ASM
+clearScreen = do
+    ld HL videoStart
+    rec loop <- label
+        ld [HL] 0x20
+        inc HL
+        ld A H
+        cp 0xc4
+        jr NZ loop
+    pure ()
