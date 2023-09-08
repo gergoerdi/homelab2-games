c6e734f71e13b5e38baf132a19cf416ffd4f88fd
Gergo ERDI <gergo@erdi.hu>
2023-09-08 13:02:15 +0200

    Generate random fruit placement

--- src/Snake/Main.hs
+++ src/Snake/Main.hs
@@ -18,8 +18,10 @@ data Locations = MkLocs
   , newHead :: Location
   , bodyDispatchTrampoline :: Location
   , bodyDispatch :: Location
-  , slitherF, lfsr11F :: Location
+  , slitherF, lfsr11F, isInBoundsF, randomizeF :: Location
   , lastInput, currentDir :: Location
+  , rng :: Location
+  , fruitLoc :: Location
   }
 
 videoStart :: Word16
@@ -39,6 +41,9 @@ wall :: Word8
 wall = 0xfb
 -- wall = 0xa0
 
+fruit :: Word8
+fruit = 0x74
+
 snake :: Z80ASM
 snake = mdo
     let locs = MkLocs{..}
@@ -49,6 +54,8 @@ snake = mdo
         initState locs
         drawSnake locs
         withLabel \loop -> do
+            drawFruit locs
+            call randomizeF
             replicateM_ 3 do
                 halt
                 call latchInputF
@@ -60,6 +67,9 @@ snake = mdo
     slitherF <- labelled $ slither locs
     latchInputF <- labelled $ latchInput locs
     lfsr11F <- labelled $ lfsr11
+    rng <- labelled $ dw [0x0101]
+    isInBoundsF <- labelled $ isInBounds locs
+    randomizeF <- labelled $ randomize locs
 
     bodyDispatchTrampoline <- labelled $ do
         ld HL [bodyDispatch]
@@ -74,6 +84,7 @@ snake = mdo
     segmentChar <- labelled $ resb 0x100
     lastInput <- labelled $ resb 1
     currentDir <- labelled $ resb 1
+    fruitLoc <- labelled $ resw 1
     pure ()
 
 clearScreen :: Z80ASM
@@ -108,6 +119,18 @@ initState MkLocs{..} = do
         inc IY
     ldVia A [segmentChar + 10] headE
 
+    withLabel \loop -> do
+        call randomizeF
+        ldVia A [fruitLoc] [rng]
+        ld L A
+        ld A [rng + 1]
+        Z80.and 0x03
+        Z80.or 0xc0
+        ld [fruitLoc + 1] A
+        ld H A
+        call isInBoundsF
+        jp Z loop
+
 -- | A 11-bit maximal LFSR
 -- | Pre: DE is the current state
 lfsr11 :: Z80ASM
@@ -121,6 +144,73 @@ lfsr11 = do
         ld D A
     ret
 
+randomize :: Locations -> Z80ASM
+randomize MkLocs{..} = do
+    ldVia A E [rng]
+    ldVia A D [rng + 1]
+    call lfsr11F
+    ldVia A [rng] E
+    ldVia A [rng + 1] D
+    ret
+
+-- | Pre: `HL` contains the address to check
+-- | Post: `Z` iff out of bounds
+isInBounds :: Locations -> Z80ASM
+isInBounds MkLocs{..} = mdo
+    -- Skip top border
+    skippable \notTop -> do
+        ld A H
+        cp 0xc0
+        jp NZ notTop
+        ld A L
+        cp (numCols + 1)
+        jp C outOfBounds
+
+    -- Skip bottom border
+    skippable \notBottom -> do
+        ld A H
+        cp 0xc3
+        jp NZ notBottom
+        ld A L
+        cp $ fromIntegral $ ((numRows - 1) * numCols + 1) `mod` 256
+        jp NC outOfBounds
+
+    -- Skip vertical borders: keep subtracting numCols until we get 0 or -1
+    push HL
+    push DE
+    ld DE (negate numCols)
+    withLabel \loop -> mdo
+        ld A H
+        cp 0xc0
+        jp NZ next
+        ld A L
+        cp numCols
+        jp Z outOfBounds'
+        jp NC next
+        cp 0x01
+        jp Z outOfBounds'
+        jp inBounds
+
+        next <- label
+        add HL DE
+        jp loop
+
+        inBounds <- labelled do
+            pop DE
+            pop HL
+            ld A 0xff
+            ret
+
+        outOfBounds' <- labelled do
+            pop DE
+            pop HL
+            jp outOfBounds
+        pure ()
+
+    outOfBounds <- labelled $ do
+        Z80.xor A
+        ret
+    pure ()
 
 gameOver :: Locations -> Z80ASM
 gameOver MkLocs{..} = skippable \end -> do
@@ -133,109 +223,51 @@ gameOver MkLocs{..} = skippable \end -> do
         jp end
 
         fill <- labelled do
-            -- ld HL (videoStart + 41)
-            -- decLoopB 23 do
-            --     push HL
-            --     exx
-            --     pop HL
-            --     decLoopB 38 do
-            --         ld [HL] A
-            --         inc HL
-            --     inc HL
-            --     inc HL
-            --     push HL
-            --     exx
-            --     pop HL
-            --     replicateM_ 2 halt
-            -- ret
-            ld DE 1
-            exx
-            ld C A
-            exx
-            decLoopB 8 do
-                push DE
-                exx
-                pop DE
-                decLoopB 0x100 $ skippable \skip -> do
-                    call lfsr11F
-
-                    ld A D
-                    Z80.and 0x03
-                    Z80.or 0xc0
-                    ld H A
-                    ld L E
-
-                    -- Skip top border
-                    skippable \notTop -> do
-                        ld A H
-                        cp 0xc0
-                        jp NZ notTop
-                        ld A L
-                        cp (numCols + 1)
-                        jp C skip
-
-                    -- Skip bottom border
-                    skippable \notBottom -> do
-                        ld A H
-                        cp 0xc3
-                        jp NZ notBottom
-                        ld A L
-                        cp $ fromIntegral $ ((numRows - 1) * numCols + 1) `mod` 256
-                        jp NC skip
-
-                    -- Skip vertical borders: keep subtracting numCols until we get 0 or -1
-                    push HL
-                    push DE
-                    ld DE (negate numCols)
-                    withLabel \loop -> do
-                        rec
-                            ld A H
-                            cp 0xc0
-                            jp NZ next
-                            ld A L
-                            cp numCols
-                            jp Z skipThis
-                            jp NC next
-                            cp 0x01
-                            jp Z skipThis
-                            jp draw
-
-                            next <- label
-                            add HL DE
-                            jp loop
-
-                            draw <- label
-                            pop DE
-                            pop HL
-                            ldVia A [HL] C
-                            jp done
-
-                            skipThis <- label
-                            pop DE
-                            pop HL
-
-                            done <- label
-                        pure ()
-                    -- pop DE
-                    -- pop HL
-
-                    -- ld IX videoStart
-                    -- ld IY (numCols - 1)
-                    -- ld A numRows
-                    -- withLabel \loop -> do
-                    --     -- If IX
-                    --     add IX IY
-
-                    --     dec A
-                    --     jp NZ loop
-
-                    -- ldVia A [HL] C
-
-                push DE
-                exx
-                pop DE
+            -- curtainV
+            scramble
             ret
     pure ()
+  where
+    curtainV = do
+        ld HL (videoStart + 41)
+        decLoopB (numRows - 2) do
+            push HL
+            exx
+            pop HL
+            decLoopB (numCols - 2) do
+                ld [HL] A
+                inc HL
+            inc HL
+            inc HL
+            push HL
+            exx
+            pop HL
+            replicateM_ 1 halt
+
+    scramble = do
+        ld DE 1
+        exx
+        ld C A
+        exx
+        decLoopB 8 do
+            push DE
+            exx
+            pop DE
+            decLoopB 0x100 $ skippable \skip -> do
+                call lfsr11F
+
+                ld A D
+                Z80.and 0x03
+                Z80.or 0xc0
+                ld H A
+                ld L E
+
+                call isInBoundsF
+                jp Z skip
+                ldVia A [HL] C
+            push DE
+            exx
+            pop DE
 
 drawBorder :: Z80ASM
 drawBorder = do
@@ -391,6 +423,12 @@ drawSnake MkLocs{..} = do
         jp Z end
         inc C
         jp loop
+
+drawFruit :: Locations -> Z80ASM
+drawFruit MkLocs{..} = do
+    ldVia A L [fruitLoc]
+    ldVia A H [fruitLoc + 1]
+    ld [HL] fruit
 
 -- | Pre: register `idx` contains the index
 -- | Post: `target` contains the value at `(base + index)`
