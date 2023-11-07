034fe9f5b36f32ecfdb45a5399f4293854a1067c
Gergo ERDI <gergo@erdi.hu>
2023-11-07 18:02:14 +0800

    Data-driven instead of code-driven dispatch between two screen regions

--- src/TVC/Hello.hs
+++ src/TVC/Hello.hs
@@ -9,7 +9,8 @@ import Control.Monad
 hello :: Z80ASM
 hello = mdo
     di
-    call int1
+    setInterruptHandler handler
+    ei
 
     loopForever $ return ()
 
@@ -18,98 +19,97 @@ hello = mdo
             crtcOut 0x0e hi
             crtcOut 0x0f lo
 
-    int1 <- labelled mdo
+    whichHalf <- labelled $ db [0]
+
+    handler <- labelled mdo
         push AF
         push HL
         out [0x07] A
 
-        -- Set border color to red
-        ld A 0b10_00_10_00
-        out [0x00] A
+        ld A [whichHalf]
+        xor 0xff
+        ld [whichHalf] A
+        jp Z half2
 
-        setInterruptHandler int2
-        setupLineInt 200
+        half1 <- labelled do
+            -- Set border color to red
+            ld A 0b10_00_10_00
+            out [0x00] A
 
+            setupLineInt 200
+            jp finish
+
+        half2 <- labelled do
+            -- Set border color to green
+            ld A 0b10_10_00_00
+            out [0x00] A
+
+            setupLineInt 160
+
+        finish <- label
         pop HL
         pop AF
         ei
         ret
 
-    int2 <- labelled do
-        push AF
-        push HL
-        out [0x07] A
+    -- -- Save current graphics settings
+    -- ld A [0x0b13]
+    -- push AF
 
-        -- Set border color to green
-        ld A 0b10_10_00_00
-        out [0x00] A
+    -- Z80.and 0b1111_1100
+    -- Z80.or  0b0000_0010 -- Graphics mode 16
+    -- -- Z80.or  0b0000_0001 -- Graphics mode 4
+    -- -- Z80.or  0b0000_0000 -- Graphics mode 2
+    -- out [0x06] A
 
-        setInterruptHandler int1
-        setupLineInt 160
+    -- -- Clear screen
+    -- syscall 0x05
 
-        pop HL
-        pop AF
-        ei
-        ret
+    -- -- -- Set interrupt
 
-    -- Save current graphics settings
-    ld A [0x0b13]
-    push AF
+    -- call pageVideoIn
+    -- ld DE videoStart
+    -- decLoopB 8 do
+    --     push BC
+    --     decLoopB 256 do
+    --         push BC
+    --         ld HL colors
+    --         ld BC 8
+    --         ldir
+    --         pop BC
+    --     pop BC
+    -- call pageVideoOut
 
-    Z80.and 0b1111_1100
-    Z80.or  0b0000_0010 -- Graphics mode 16
-    -- Z80.or  0b0000_0001 -- Graphics mode 4
-    -- Z80.or  0b0000_0000 -- Graphics mode 2
-    out [0x06] A
+    -- -- Wait for keypress
+    -- syscall 0x91
 
-    -- Clear screen
-    syscall 0x05
+    -- -- Resore video mode
+    -- pop AF
+    -- out [0x06] A
 
-    -- -- Set interrupt
+    -- ret
 
-    call pageVideoIn
-    ld DE videoStart
-    decLoopB 8 do
-        push BC
-        decLoopB 256 do
-            push BC
-            ld HL colors
-            ld BC 8
-            ldir
-            pop BC
-        pop BC
-    call pageVideoOut
+    -- colors <- labelled $ db
+    --   [ 0b1101_1000 -- RG
+    --   , 0b1100_0010 -- BK
+    --   , 0b1111_1101
+    --   , 0b1101_1011
+    --   , 0b0000_0000
+    --   , 0b0000_0000
+    --   , 0b0000_0000
+    --   , 0b0000_0000
+    --   ]
 
-    -- Wait for keypress
-    syscall 0x91
+    -- pageVideoIn <- labelled do
+    --     ld A 0x50
+    --     ld [0x03] A
+    --     out [0x02] A
+    --     ret
 
-    -- Resore video mode
-    pop AF
-    out [0x06] A
-
-    ret
-
-    colors <- labelled $ db
-      [ 0b1101_1000 -- RG
-      , 0b1100_0010 -- BK
-      , 0b1111_1101
-      , 0b1101_1011
-      , 0b0000_0000
-      , 0b0000_0000
-      , 0b0000_0000
-      , 0b0000_0000
-      ]
-
-    pageVideoIn <- labelled do
-        ld A 0x50
-        ld [0x03] A
-        out [0x02] A
-        ret
-
-    pageVideoOut <- labelled do
-        ld A 0x70
-        ld [0x03] A
-        out [0x02] A
-        ret
+    -- pageVideoOut <- labelled do
+    --     ld A 0x70
+    --     ld [0x03] A
+    --     out [0x02] A
+    --     ret
 
     pure ()
