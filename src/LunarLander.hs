660b9e54affcde5202c9c7964dee073dab3b9257
Gergo ERDI <gergo@erdi.hu>
2024-02-12 16:15:49 +0800

    Lunar Lander: Implement directional sprites

--- src/LunarLander.hs
+++ src/LunarLander.hs
@@ -11,8 +11,19 @@ game = mdo
     di
     pageIO
 
+    let setPic pic = do
+            ld IX landerPic
+            ld HL pic
+            ld [IX] L
+            ld [IX + 1] H
+
     call clearScreen
     loopForever do
+        ld HL frame
+        inc [HL]
+
+        call clearLander
+
         -- Apply gravity every second frame
         ld A [frame]
         Z80.and 0x01
@@ -21,6 +32,8 @@ game = mdo
             ld DE 0x00_01
             add HL DE
             ld [landerVY] HL
+
+        setPic landerPicIdle
         call scanKeys
         call moveLander
         call drawLander
@@ -38,12 +51,20 @@ game = mdo
             jp Z waitFrame2
 
     clearScreen <- labelled clearScreen_
+    frame <- labelled $ db [0]
     landerX <- labelled $ dw [30 * 256]
     landerY <- labelled $ dw [1 * 256]
     landerVX <- labelled $ dw [0]
     landerVY <- labelled $ dw [0]
 
-    landerPic <- labelled $ db $ concat lander
+    landerPicIdle <- labelled $ db $ concat landerIdle
+    landerPicDown1 <- labelled $ db $ concat landerDown1
+    landerPicDown2 <- labelled $ db $ concat landerDown2
+    landerPicRight1 <- labelled $ db $ concat landerRight1
+    landerPicRight2 <- labelled $ db $ concat landerRight2
+    landerPicLeft1 <-  labelled $ db $ concat landerLeft1
+    landerPicLeft2 <- labelled $ db $ concat landerLeft2
+    landerPic <- labelled $ dw [landerPicIdle]
 
     moveLander <- labelled do
         ld HL [landerX]
@@ -64,13 +85,25 @@ game = mdo
                 rra
                 jp C notThis
                 body
-                ret
+                -- ret
+
+        let setPic2 pic1 pic2 = do
+                skippable \end -> do
+                    ld B A
+                    ld A [frame]
+                    Z80.and 0x01
+                    unlessFlag Z do
+                        setPic pic1
+                        jp end
+                    setPic pic2
+                ld A B
 
         ld HL [landerVY]
         checkKey do -- Down
             ld DE $ negate 0x00_02
             add HL DE
             ld [landerVY] HL
+            setPic2 landerPicDown1 landerPicDown2
 
         checkKey do -- Up
             ld DE 0x00_02
@@ -82,11 +115,13 @@ game = mdo
             ld DE $ negate 0x00_01
             add HL DE
             ld [landerVX] HL
+            setPic2 landerPicRight1 landerPicRight2
 
         checkKey do -- Left
             ld DE 0x00_01
             add HL DE
             ld [landerVX] HL
+            setPic2 landerPicLeft1 landerPicLeft2
 
         ret
 
@@ -104,10 +139,16 @@ game = mdo
         ret
 
     drawLander <- labelled do
+        ld IX landerPic
+        ld L [IX]
+        ld H [IX + 1]
+        push HL
+        pop IX
+
         call landerScreenAddr
         ld DE $ rowstride - 5
-        ld IX landerPic
-        decLoopB 4 do
+        -- ld IX landerPic
+        decLoopB 5 do
             ld C B
             decLoopB 5 do
                 ldVia A [HL] [IX]
@@ -121,7 +162,7 @@ game = mdo
         call landerScreenAddr
         ld DE $ rowstride - 5
         ld A 0x00
-        decLoopB 4 do
+        decLoopB 5 do
             ld C B
             decLoopB 5 do
                 ld [HL] A
@@ -143,10 +184,65 @@ clearScreen_ = do
         ld B C
     ret
 
-lander :: [[Word8]]
-lander =
+landerIdle :: [[Word8]]
+landerIdle =
     [ [ 0x00, 0x1a, 0x12, 0x1e, 0x00 ]
     , [ 0x00, 0xea, 0x14, 0xd5, 0x00 ]
     , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
     , [ 0x18, 0x1e, 0x00, 0x1d, 0x15 ]
+    , [ 0x00, 0x00, 0x00, 0x00, 0x00 ]
+    ]
+
+landerDown1 :: [[Word8]]
+landerDown1 =
+    [ [ 0x00, 0x1a, 0x12, 0x1e, 0x00 ]
+    , [ 0x00, 0xea, 0x14, 0xd5, 0x00 ]
+    , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
+    , [ 0x18, 0x1e, 0xd9, 0x1d, 0x15 ]
+    , [ 0x00, 0xe6, 0xe6, 0xe6, 0x00 ]
+    ]
+
+landerDown2 :: [[Word8]]
+landerDown2 =
+    [ [ 0x00, 0x1a, 0x12, 0x1e, 0x00 ]
+    , [ 0x00, 0xea, 0x14, 0xd5, 0x00 ]
+    , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
+    , [ 0x18, 0x1e, 0xe6, 0x1d, 0x15 ]
+    , [ 0x00, 0xd9, 0xd9, 0xd9, 0x00 ]
+    ]
+
+landerRight1 :: [[Word8]]
+landerRight1 =
+    [ [ 0x00, 0x1a, 0x12, 0x1e, 0x1e ]
+    , [ 0x00, 0xea, 0x14, 0xd5, 0x19 ]
+    , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
+    , [ 0x18, 0x1e, 0x00, 0x1d, 0x15 ]
+    , [ 0x00, 0x00, 0x00, 0x00, 0x00 ]
+    ]
+
+landerRight2 :: [[Word8]]
+landerRight2 =
+    [ [ 0x00, 0x1a, 0x12, 0x1e, 0x1d ]
+    , [ 0x00, 0xea, 0x14, 0xd5, 0x1a ]
+    , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
+    , [ 0x18, 0x1e, 0x00, 0x1d, 0x15 ]
+    , [ 0x00, 0x00, 0x00, 0x00, 0x00 ]
+    ]
+
+landerLeft1 :: [[Word8]]
+landerLeft1 =
+    [ [ 0x1d, 0x1a, 0x12, 0x1e, 0x00 ]
+    , [ 0x1a, 0xea, 0x14, 0xd5, 0x00 ]
+    , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
+    , [ 0x18, 0x1e, 0x00, 0x1d, 0x15 ]
+    , [ 0x00, 0x00, 0x00, 0x00, 0x00 ]
+    ]
+
+landerLeft2 :: [[Word8]]
+landerLeft2 =
+    [ [ 0x1e, 0x1a, 0x12, 0x1e, 0x00 ]
+    , [ 0x19, 0xea, 0x14, 0xd5, 0x00 ]
+    , [ 0x00, 0x14, 0x14, 0x14, 0x00 ]
+    , [ 0x18, 0x1e, 0x00, 0x1d, 0x15 ]
+    , [ 0x00, 0x00, 0x00, 0x00, 0x00 ]
     ]
