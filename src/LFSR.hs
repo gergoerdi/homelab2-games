4877353e91188688c7dca8b402e4c2dd400a3c42
Gergo ERDI <gergo@erdi.hu>
2024-02-23 18:31:28 +0800

    Add an 11-bit maximal LFSR

--- src/LFSR.hs
+++ src/LFSR.hs
@@ -1,23 +1,38 @@
-{-# LANGUAGE BlockArguments #-}
-module LFSR where
+module LFSR (lfsr10, lfsr11) where
 
 import Z80
 import Z80.Utils
+import Data.Word
+
+-- | Pre: `DE` is the current state
+-- | Post: `DE` is the new state
+-- | Clobbers: `A`
+lfsr :: Word16 -> Z80ASM
+lfsr coeffs = do
+    srl D
+    ld A E
+    rra
+    ld E A
+    ret NC
+    Z80.xor lo
+    ld E A
+    ld A hi
+    Z80.xor D
+    ld D A
+    ret
+  where
+    (lo, hi) = wordBytes coeffs
 
 -- | An 10-bit maximal LFSR
 -- | Pre: `DE` is the current state
 -- | Post: `DE` is the new state
 -- | Clobbers: `A`
 lfsr10 :: Z80ASM
-lfsr10 = do
-    srl D
-    ld A E
-    rra
-    ld E A
-    ret NC
-    Z80.xor 0x04
-    ld E A
-    ld A 0x02
-    Z80.xor D
-    ld D A
-    ret
+lfsr10 = lfsr 0x0204
+
+-- | An 11-bit maximal LFSR
+-- | Pre: `DE` is the current state
+-- | Post: `DE` is the new state
+-- | Clobbers: `A`
+lfsr11 :: Z80ASM
+lfsr11 = lfsr 0x0402
